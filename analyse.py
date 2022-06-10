abbdc8456435f2c6fe7d9599c7272adb376d27ca
KernelErr <me@lirui.tech>
2022-06-10 17:49:39 +0800

    Add analyse script

---
+++ analyse.py
@@ -0,0 +1,83 @@
+import os
+import glob
+import toml
+import pymongo
+
+MONGODB = os.getenv("MONGODB")
+
+if MONGODB is None:
+    raise Exception("MONGODB environment variable not set")
+
+client = pymongo.MongoClient(MONGODB)
+db = client.rsad
+
+rust_count = 0
+crates_count = 0
+
+files = glob.glob("rust/**/*.md")
+for file in files:
+    with open(file, 'r') as f:
+        project = file.split('/')[1]
+        id = file.split('/')[2].split('.')[0]
+        print("Processing {} - {}".format(project, id))
+        try:
+            contents = f.readlines()
+            toml_contents = ""
+            for line in contents:
+                if line == "```toml\n":
+                    continue
+                if line == "```\n":
+                    break
+                toml_contents += line
+            raw_data = toml.loads(toml_contents)
+            data = {}
+            if "advisory" in raw_data:
+                for (key, value) in raw_data["advisory"].items():
+                    data["advisory_{}".format(key)] = value
+            if "affected" in raw_data:
+                for (key, value) in raw_data["affected"].items():
+                    data["affected_{}".format(key)] = value
+            if "versions" in raw_data:
+                for (key, value) in raw_data["versions"].items():
+                    data["versions_{}".format(key)] = value
+            if db.rust.find_one({"advisory_id": id}) is None:
+                db.rust.insert_one(data)
+            rust_count += 1
+        except Exception as e:
+            print("Failed to process {}: {}".format(id, e))
+            continue
+
+files = glob.glob("crates/**/*.md")
+for file in files:
+    with open(file, 'r') as f:
+        crate = file.split('/')[1]
+        id = file.split('/')[2].split('.')[0]
+        print("Processing {} - {}".format(crate, id))
+        try:
+            contents = f.readlines()
+            toml_contents = ""
+            for line in contents:
+                if line == "```toml\n":
+                    continue
+                if line == "```\n":
+                    break
+                toml_contents += line
+            raw_data = toml.loads(toml_contents)
+            data = {}
+            if "advisory" in raw_data:
+                for (key, value) in raw_data["advisory"].items():
+                    data["advisory_{}".format(key)] = value
+            if "affected" in raw_data:
+                for (key, value) in raw_data["affected"].items():
+                    data["affected_{}".format(key)] = value
+            if "versions" in raw_data:
+                for (key, value) in raw_data["versions"].items():
+                    data["versions_{}".format(key)] = value
+            if db.crates.find_one({"advisory_id": id}) is None:
+                db.crates.insert_one(data)
+            crates_count += 1
+        except Exception as e:
+            print("Failed to process {}: {}".format(id, e))
+            continue
+
+print("Processed {} Rust advisories and {} crates advisories".format(rust_count, crates_count))
